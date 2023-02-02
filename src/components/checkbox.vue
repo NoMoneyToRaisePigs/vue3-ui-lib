a2ccabee2c0d55de56874190edd3117d8eb8ee5e
NoMoneyToRaisePigs <gaofan19901103@gmail.com>
2023-02-02 22:35:46 +0800

    test lint-staged again

--- src/components/checkbox.vue
+++ src/components/checkbox.vue
@@ -53,7 +53,7 @@ const props = withDefaults(defineProps<CheckboxProps>(), {
   size: 'medium',
 })
 
-const innerValue = ref<CheckBoxModelValue>(false)
+const innerValue = ref(false)
 
 const emit = defineEmits<{
   (event:UPDATE_MODEL_VALUE_TYPE, value: CheckBoxModelValue): void
